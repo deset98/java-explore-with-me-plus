01ac3775426490a7c5ccd1e03e9444369ddbf4db
berbenets_m <berbenets.maksim@bk.ru>
2025-09-10 17:40:33 +0300

    добавлен сервис и репозиторий для работы с бд

--- stats/stats-server/src/main/java/ru/practicum/ewm/service/StatsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/service/StatsServiceImpl.java
@@ -1,12 +1,18 @@
 package ru.practicum.ewm.service;
 
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import ru.practicum.ewm.RequestHitDto;
+import ru.practicum.ewm.RequestStatDto;
+import ru.practicum.ewm.StatDto;
 import ru.practicum.ewm.entity.RequestHit;
 import ru.practicum.ewm.repository.RequestHitRepository;
-import ru.practicum.ewm.repository.StatRepository;
 
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -14,16 +20,29 @@ import java.util.List;
 public class StatsServiceImpl implements StatsService {
 
     private final RequestHitRepository requestHitRepository;
+    private final AAAA mapper;
 
     @Override
     public ResponseEntity<Void> createEndpointHit(RequestHitDto requestHitDto) {
-        RequestHit savedHit = mapper.toEntity(requestHitDto);
-        requestHitRepository.save(savedHit);
-        return ResponseEntity.created("/hit").build();
+        RequestHit hitEntity = mapper.toEntity(requestHitDto);
+        RequestHit savedHit = requestHitRepository.save(hitEntity);
+        return ResponseEntity.created(URI.create("/hit/" + savedHit.getId())).build();
     }
 
     @Override
     public List<StatDto> getViewStats(String start, String end, String app, List<String> uris, Boolean unique) {
+        RequestStatDto requestStatDto = new RequestStatDto(LocalDateTime.parse(start, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), LocalDateTime.parse(end, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), uris, unique);
+        return getViewStats(requestStatDto, app);
+    }
 
+    private List<StatDto> getViewStats(RequestStatDto dto, String app) {
+        if (!dto.isUnique()) {
+            throw new ValidationException("End date must be after start date");
+        }
+        if (dto.isUnique()) {
+            return requestHitRepository.findUniqueStats(dto.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), dto.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), app, dto.getUris());
+        } else {
+            return requestHitRepository.findUniqueStats(dto.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), dto.getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), app, dto.getUris());
+        }
     }
 }
