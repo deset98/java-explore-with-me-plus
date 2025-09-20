d72392d69a02137eaf7cb537259304ccf78cadbd
deset98 <deset.98@ya.ru>
2025-09-20 16:33:57 +0300

    добавлен эндпоинт /users/{userId}/events

--- main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -1,40 +1,54 @@
 package ru.practicum.ewm.event.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.category.model.Category;
+import ru.practicum.ewm.category.repository.CategoryRepository;
 import ru.practicum.ewm.event.dto.EventFullDto;
 import ru.practicum.ewm.event.dto.NewEventDto;
 import ru.practicum.ewm.event.mapper.EventMapper;
 import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.event.repository.EventRepository;
+import ru.practicum.ewm.exception.NotFoundException;
+import ru.practicum.ewm.user.model.User;
+import ru.practicum.ewm.user.repository.UserRepository;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
-    private final EventMapper eventMapper;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
 
-    public EventServiceImpl(final EventRepository eventRepository,
-                            final EventMapper eventMapper) {
-        this.eventRepository = eventRepository;
-        this.eventMapper = eventMapper;
-    }
+    private final EventMapper eventMapper;
 
     @Transactional
     public EventFullDto create(Long userId, final NewEventDto newEventDto) {
         log.debug("В EventServiceImpl вызван метод для создания события");
 
-        Event event = eventMapper.
+        Event event = eventMapper.toEntity(newEventDto);
+        this.setCategoryAndInitiator(newEventDto.getCategoryId(), userId, event);
+        eventRepository.save(event);
+        return eventMapper.toFullDto(event);
+    }
 
-        // 1. Собрать сущность Event
-        // 1.1. Подставить Category
-        // 1.2. Подставить User
-        // 2. Сохранить ее в репозиторий
-        // 3. Вернуть FullDto
 
-        return EventFullDto.builder().build();
+    private void setCategoryAndInitiator(Long categoryId, Long userId, Event event) {
+        Category category = categoryRepository.findById(newEventDto.getCategoryId())
+                .orElseThrow(() -> new NotFoundException(
+                        "Объект Category id={} не найден", newEventDto.getCategoryId()
+                ));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(
+                        "Объект User id={} не найден", userId
+                ));
+
+        event.setCategory(category);
+        event.setInitiator(user);
     }
 }
