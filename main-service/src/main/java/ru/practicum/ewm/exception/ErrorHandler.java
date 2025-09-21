513470760cd45b49ec087c64a284ade066647264
DmitrySol <sockolov.44itya@yandex.ru>
2025-09-21 10:06:52 +0300

    подготовка к слиянию

--- main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.exception;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
